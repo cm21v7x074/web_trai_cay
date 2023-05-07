07896813cde0a5d6de688971416fcd42cce2d206
cm21v7x074 <128952077+cm21v7x074@users.noreply.github.com>
2023-05-07 10:06:53 +0700

    update root

---
+++ public/delete_quote.php
@@ -0,0 +1,58 @@
+<?php
+
+define('TITLE', 'Xóa một Trích dẫn');
+include '../partials/header.php';
+
+echo '<h2>Xóa một Trích dẫn</h2>';
+
+include '../partials/check_admin.php';
+
+include '../partials/db_connect.php';
+
+if (isset($_GET['id']) && is_numeric($_GET['id']) && ($_GET['id'] > 0)) {
+    $query = "SELECT * FROM quotes WHERE id={$_GET['id']}";
+    try {
+        $statement = $pdo->query($query);
+        $row = $statement->fetch();
+    } catch (PDOException $e) {
+        $pdo_error = $e->getMessage();
+    }
+
+    if (!empty($row)){
+        echo '<form action="delete_quote.php" method="post">
+            <p>Bạn có muốn xóa trích dẫn này?</p>
+            <div><blockquote>'. htmlspecialchars($row['quote']) .'</blockquote> - '. htmlspecialchars($row['source']);
+            if ($row['favorite'] == 1){
+                echo ' <strong>Yêu thích!</strong>';
+            }
+        echo '</div><br>
+            <input type="hidden" name="id" value="'. $_GET['id'] .'" >
+            <p><input type="submit" name="submit" value="Xóa Trích dẫn này!" ></p>
+        </form>';
+    } else {
+        $error_message = 'Không thể lấy được trích dẫn';
+        $reason = $pdo_error ?? 'Không rõ nguyễn nhân';
+        include '../partials/show_error.php';
+    }
+} elseif (isset($_POST['id']) && is_numeric($_POST['id']) && ($_POST['id'] > 0)) {
+    $query = "DELETE FROM quotes WHERE id={$_POST['id']}";
+
+    try {
+        $statement = $pdo->query($query);
+    } catch (PDOException $e) {
+        $pdo_error = $e->getMessage();
+    }
+
+    if ($statement && $statement->rowCount() == 1){
+        echo '<p>Trích dẫn đã được xóa!</p>';
+    } else {
+        $error_message = 'Không thể xóa trích dẫn';
+        $reason = $pdo_error ?? 'Không rõ nguyễn nhân';
+        include '../partials/show_error.php';
+    }
+} else {
+    include '../partials/show_error.php';
+}
+
+include '../partials/footer.php';
+?>
---
+++ public/view_quotes.php
@@ -0,0 +1,29 @@
+<?php
+
+define('TITLE', 'Xem tất cả các Trích dẫn');
+include '../partials/header.php';
+
+echo '<h2>Tất cả các Trích dẫn</h2>';
+
+include '../partials/check_admin.php';
+
+// echo '<p>Trang đang được xây dựng...</p>';
+include '../partials/db_connect.php';
+$query = 'SELECT * FROM quotes ORDER BY date_entered DESC';
+
+try {
+    foreach ($pdo->query($query) as $row) {
+        echo "<div><blockquote>" . htmlspecialchars($row['quote']) . "</blockquote>-" . htmlspecialchars($row['source']) . "<br>";
+        if ($row['favorite'] == 1){
+            echo '<strong>Yêu thích</strong>';
+        }
+        echo "<p><b>Quản trị Trích dẫn:<b> <a href='edit_quote.php?id={$row["id"]}'>Sửa</a> <a href='delete_quote.php?id={$row["id"]}'>Xóa</a></p></div><br>";
+    }
+} catch (PDOException $e) {
+    $error_message = 'Không thể lấy dữ liệu';
+    $reason = $e->getMessage();
+    include '../partials/show_error.php';
+}
+
+include '../partials/footer.php';
+?>
